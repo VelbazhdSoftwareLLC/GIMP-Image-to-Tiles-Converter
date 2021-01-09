17934d9f84e0bfee093f28cb88d47b770863de5e
Todor Balabanov <todor.balabanov@gmail.com>
2021-01-09 16:44:48 +0200

    Image resizing was done.

--- Image-to-Tiles-Converter.py
+++ Image-to-Tiles-Converter.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
  
 from gimpfu import *
- 
+from math import sqrt, ceil
+
+def dimensions_as_tiles(width, height, tiles):
+	tile_area = width * height / tiles
+
+	tile_side = ceil(sqrt( tile_area ))
+
+	width_in_tiles = ceil(width / tile_side)
+	height_in_tiles = ceil(height / tile_side)
+
+	return (width_in_tiles, height_in_tiles, tile_side)
+
 def plugin_main(image, drawable, number_of_tiles):
-	gimp.message("Hi all!")
+	''' Calculate dimensions as number of tiles and tiles size. '''
+	(x_tiles, y_tiles, tile_side_length) = dimensions_as_tiles(image.width, image.height, number_of_tiles)
+	#gimp.message( "".join(str((number_of_tiles, x_tiles, y_tiles, tile_side_length))) )
+
+	''' Calculate image resize parameters. '''
+	number_of_tiles = x_tiles * y_tiles
+	image_new_width = x_tiles * tile_side_length
+	image_new_height = y_tiles * tile_side_length
+	#gimp.message( "".join(str((number_of_tiles, image.width, image.height, image_new_width, image_new_height))) )
+
+	''' Resize image.  '''
+	pdb.gimp_context_set_interpolation(INTERPOLATION_LANCZOS)
+	pdb.gimp_image_scale(image, image_new_width, image_new_height)
  
 register(
 	"python_fu_image_to_tiles",
