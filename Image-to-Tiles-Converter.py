2b0296778e35500273b7e69e051e917778f48b58
Todor Balabanov <todor.balabanov@gmail.com>
2021-01-10 18:15:10 +0200

    Sample file was added.

--- Image-to-Tiles-Converter.py
+++ Image-to-Tiles-Converter.py
@@ -13,15 +13,16 @@ def dimensions_as_tiles(width, height, tiles):
 
 	return (width_in_tiles, height_in_tiles, tile_side)
 
+def image_setup(x, y, length):
+	return (x*y, x*length, y*length)
+
 def plugin_main(image, drawable, number_of_tiles):
 	''' Calculate dimensions as number of tiles and tiles size. '''
 	(x_tiles, y_tiles, tile_side_length) = dimensions_as_tiles(image.width, image.height, number_of_tiles)
 	#gimp.message( "".join(str((number_of_tiles, x_tiles, y_tiles, tile_side_length))) )
 
 	''' Calculate image resize parameters. '''
-	number_of_tiles = x_tiles * y_tiles
-	image_new_width = x_tiles * tile_side_length
-	image_new_height = y_tiles * tile_side_length
+	(number_of_tiles, image_new_width, image_new_height) = image_setup(x_tiles, y_tiles, tile_side_length)
 	#gimp.message( "".join(str((number_of_tiles, image.width, image.height, image_new_width, image_new_height))) )
 
 	''' Resize image.  '''
@@ -36,7 +37,7 @@ register(
 	"Velbazhd Software LLC\nGPLv3 License",
 	"2021",
 	"<Image>/Image/Custom/Image to Tiles Converter",
-	"*",
+	"RGB",
 	[
 		(PF_INT32, "number_of_tiles", "Desired Number of Tiles", 1),
 	],
