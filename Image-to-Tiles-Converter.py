f141fcd3159b2b14d78c77b8f092e094834822c8
Todor Balabanov <todor.balabanov@gmail.com>
2021-01-12 18:10:19 +0200

    Eclipse project was added.

--- Image-to-Tiles-Converter.py
+++ Image-to-Tiles-Converter.py
@@ -4,42 +4,47 @@ from gimpfu import *
 from math import sqrt, ceil
 from array import array
 
+
 def dimensions_as_tiles(width, height, tiles):
 	tile_area = width * height / tiles
 
-	tile_side = ceil(sqrt( tile_area ))
+	tile_side = ceil(sqrt(tile_area))
 
 	width_in_tiles = ceil(width / tile_side)
 	height_in_tiles = ceil(height / tile_side)
 
 	return (width_in_tiles, height_in_tiles, tile_side)
 
+
 def image_setup(x, y, length):
-	return (x*y, x*length, y*length)
+	return (x * y, x * length, y * length)
+
 
 def list_of_colors(layer):
 	colors = set()
 	for y in range(layer.height):
 		for x in range(layer.width):
-			colors.add( layer.get_pixel(x, y) )
+			colors.add(layer.get_pixel(x, y))
 	return colors
+
 
 def plugin_main(image, drawable, number_of_tiles):
 	''' Calculate dimensions as number of tiles and tiles size. '''
 	(x_tiles, y_tiles, tile_side_length) = dimensions_as_tiles(image.width, image.height, number_of_tiles)
-	#gimp.message( "".join(str((number_of_tiles, x_tiles, y_tiles, tile_side_length))) )
+	# gimp.message( "".join(str((number_of_tiles, x_tiles, y_tiles, tile_side_length))) )
 
 	''' Calculate image resize parameters. '''
 	(number_of_tiles, image_new_width, image_new_height) = image_setup(x_tiles, y_tiles, tile_side_length)
-	#gimp.message( "".join(str((number_of_tiles, image.width, image.height, image_new_width, image_new_height))) )
+	# gimp.message( "".join(str((number_of_tiles, image.width, image.height, image_new_width, image_new_height))) )
 
 	''' Resize image.  '''
 	pdb.gimp_context_set_interpolation(INTERPOLATION_LANCZOS)
 	pdb.gimp_image_scale(image, image_new_width, image_new_height)
 
 	''' Determine colors to use.  '''
-	colors = list_of_colors( image.layers[1] )
-	#gimp.message( "".join(str(colors)) )
+	colors = list_of_colors(image.layers[1])
+	# gimp.message( "".join(str(colors)) )
+
  
 register(
 	"python_fu_image_to_tiles",
